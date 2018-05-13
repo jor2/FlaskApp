ef2566d70d9024a738cdacaae1bbc8f1c4fba318
jor2 <jordanwilliams997@hotmail.com>
2018-05-14 00:38:10 +0100

    Writing user details to MySQL DB

---
+++ data.py
@@ -0,0 +1,25 @@
+def articles():
+    pieces = [
+        {
+            'id': 1,
+            'title': 'Artice One',
+            'body': 'Mock text',
+            'author': 'Jordan Williams',
+            'create_date': '04-05-1997'
+        },
+        {
+            'id': 2,
+            'title': 'Artice Two',
+            'body': 'Mock text',
+            'author': 'Abbie Williams',
+            'create_date': '02-03-2011'
+        },
+        {
+            'id': 3,
+            'title': 'Artice Three',
+            'body': 'Mock text',
+            'author': 'Margaret Williams',
+            'create_date': '03-04-2016'
+        },
+    ]
+    return pieces
